4bf0086c56206e9c152a16c9072f163ee22ecd61
Michael Wallner <mike@php.net>
2019-03-08 11:39:20 +0100

    add update script

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# pull the app and all refs
+git submodule foreach "git pull -qr origin master"
+
+cd refs
+for ref in http pq; do 
+	../mdref/bin/ref2stub $ref >/dev/null
+
+	# this is semantically awful
+	if ! diff -q {,$ref/}$ref.stub.php; then
+		cp {,$ref/}$ref.stub.php
+		(cd $ref && git commit -am "update live stub" && git push)
+	fi
+done
+cd ..
